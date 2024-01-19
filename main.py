deb863ffc5cd2edd84d7445262c4fddc07a9d70c
chiiaaraaf <89153535+chiiaaraaf@users.noreply.github.com>
2024-01-19 17:58:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ fig = go.Figure(data=[
     )
 ])
 
-# Customize layout
+
 fig.update_layout(
     title_text='Political support among parties in Portugal, 2020',
     xaxis=dict(
@@ -29,19 +29,19 @@ fig.update_layout(
         showgrid=False,
         showticklabels=True,
         tickangle=0,
-        tickfont=dict(size=12),
+        tickfont=dict(size=12, color='black'),  # Set tick label color to black
+        title_font=dict(color='black'),  # Set axis title color to black
     ),
     yaxis=dict(
         title="Percentage of support (%)",
         showgrid=False,
-        range=[0, max(percentages) * 1.2],  # Increased y-axis limit
+        range=[0, max(percentages) * 1.2],
         showticklabels=True,
         tickangle=0,
-        tickfont=dict(size=12),
+        tickfont=dict(size=12, color='black'),  # Set tick label color to black
+        title_font=dict(color='black'),  # Set axis title color to black
     ),
-    plot_bgcolor='rgba(0,0,0,0)',
-    bargap=0.05  # Significantly reduced gap between bars
+    plot_bgcolor='rgba(255,255,255,1)',  # Set background color to white
+    bargap=0.05
 )
 
-# Display the plot in Streamlit
-st.plotly_chart(fig)
